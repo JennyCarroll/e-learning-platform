dfbe0cd7fd83ec0ae01ba575eeae6a4e57c24837
JennyCarroll <jennygc@telus.net>
2023-05-28 23:34:20 +0000

    change order of config statements

--- config/application.rb
+++ config/application.rb
@@ -8,10 +8,11 @@ Bundler.require(*Rails.groups)
 
 module ELearningPlatform
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     
-    config.assets.initialize_on_precompile = false
+    
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
